285925f97fa1a924926e7083fcb4d1ee5a711a82
Arsenic <vector2912@gmail.com>
2020-10-24 14:21:05 +0530

    Technical Document Completed

--- app.js
+++ app.js
@@ -1,4 +1,21 @@
 function myFunc() {
-	cosole.log('This is my function')
+    cosole.log('This is my function')
 }
 
+var app = require('express')();
+var http = require('http').createServer(app);
+app.get('/', (req, res) => {
+            res.send('
+                Hello world ');
+            }); http.listen(3000, () => {
+            console.log('listening on *:3000');
+        });
+
+
+
+        io.on('connection', (socket) => {
+            console.log('a user connected');
+            socket.on('disconnect', () => {
+                console.log('user disconnected');
+            });
+        });
