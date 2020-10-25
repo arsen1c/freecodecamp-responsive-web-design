366d6e5814d69637bb41a97051c43008ea2eacaf
Arsenic <vector2912@gmail.com>
2020-10-25 18:36:05 +0530

    Project Completed

---
+++ Personal-portfolio/app.js
@@ -0,0 +1,7 @@
+ ScrollReveal('.projects-section', { reset: true });
+
+ScrollReveal().reveal('#welcome-section', { origin: 'bottom', distance:'100px', opacity: 0, delay: 500, easing: 'ease-out' })
+
+ScrollReveal().reveal('.projects-section', { origin: 'left', distance:'100px', opacity: 0, delay: 1000, easing: 'ease-out'})
+
+ScrollReveal().reveal('#contact', { origin: 'right', distance:'100px', opacity: 0, delay: 200, easing: 'ease-out'})
